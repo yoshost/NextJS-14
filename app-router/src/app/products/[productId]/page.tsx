4dbc56e4acc484d8ada7af3e2a6ae45661e93fed
DevYmA <ymamarathunga@gmail.com>
2024-09-17 18:39:18 +0530

    routing metadata

--- app-router/src/app/products/[productId]/page.tsx
+++ app-router/src/app/products/[productId]/page.tsx
@@ -1,10 +1,26 @@
-export default function PageDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function PageDetails({ params }: Props) {
   return (
     <>
       <h1>This is details of product : {params.productId}</h1>
