1eebf938f2518754cc91b4f23f88986d55bffe08
DevYmA <ymamarathunga@gmail.com>
2024-09-17 18:45:58 +0530

    title metatdata

--- app-router/src/app/about/page.tsx
+++ app-router/src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    title : {
+        absolute : "Override"
+    }
+}
+
 export default function About(){
     return <div>
         <h1>About page</h1>
--- app-router/src/app/blog/page.tsx
+++ app-router/src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    title : "Blog"
+}
+
 export default function Blog(){
     return <>
         <h1>This is blog</h1>
