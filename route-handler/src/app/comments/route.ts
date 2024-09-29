ff227fc9c58fd08f8432363257b33e2ba1ce6f71
DevYmA <ymamarathunga@gmail.com>
2024-09-29 07:40:05 +0530

    redirect and query param

--- route-handler/src/app/comments/route.ts
+++ route-handler/src/app/comments/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const query = request.nextUrl.searchParams.get("query");
+
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
