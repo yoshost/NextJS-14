9977e103f5e989fa3bdbb777e66c582bbbfc2395
DevYmA <ymamarathunga@gmail.com>
2024-09-30 07:22:47 +0530

    cookies

--- route-handler/src/app/profile/api/route.ts
+++ route-handler/src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-  return new Response("Profile data");
+import { cookies, headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeader = new Headers(request.headers);
+  const headersList = headers();
+
+  console.log("Request Header", requestHeader.get("Authorization"));
+  console.log("Header List", headersList.get("Authorization"));
+
+  console.log("Cookie from request : ", request.cookies.get("lang"));
+
+  cookies().set("my-set", "settings");
+
+  const cookie = cookies().get("my-set");
+  console.log("Cookie from Next Cookies : ", request.cookies.get("my-set"));
+
+  return new Response("<h1>Profile data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "lang=val1",
+    },
+  });
 }
