3320ea9af7401c020f2a0129b0f92266f8180259
DevYmA <ymamarathunga@gmail.com>
2024-10-05 17:21:28 +0530

    request cache

--- data-fetching/src/app/users/page.tsx
+++ data-fetching/src/app/users/page.tsx
@@ -6,7 +6,20 @@ type User = {
 
 export default async function Users() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch("https://jsonplaceholder.typicode.com/users234");
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users234",
+    {
+      next: {
+        revalidate: 10,
+      },
+    }
+  );
+  //   const response = await fetch(
+  //     "https://jsonplaceholder.typicode.com/users234",
+  //     {
+  //       cache: "no-store",
+  //     }
+  //   );
   const users = await response.json();
 
   return (
