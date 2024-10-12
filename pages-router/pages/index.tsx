c00684dc9e1bddb915cba17b4cc469a7315ab685
DevYmA <ymamarathunga@gmail.com>
2024-10-12 16:10:01 +0530

    inital of page router

---
+++ pages-router/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>Hello world!</h1>
+      </main>
+    </>
+  );
+}
