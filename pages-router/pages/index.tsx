01b7cd96073f9b9bfcc0a748f84d59f439c49e8e
DevYmA <ymamarathunga@gmail.com>
2024-10-16 08:13:34 +0530

    link programtic router and 404

--- pages-router/pages/index.tsx
+++ pages-router/pages/index.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const clickHandler = () => {
+    console.log("clicked");
+    router.push("/product");
+  };
   return (
     <>
       <main>
         <h1>Hello world!</h1>
         <Link href="/blog">Blog</Link>
         <Link href="/product">Product</Link>
+        <button onClick={clickHandler}>Place Order</button>
       </main>
     </>
   );
